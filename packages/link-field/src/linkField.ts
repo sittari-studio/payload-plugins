6743347f3aa7a98531eb455aaca455e5b60c6123
roxxel <64275777+roxxel@users.noreply.github.com>
2026-08-22 05:22:36 +0300

    fix(link-field): add localizeLabel option

--- packages/link-field/src/linkField.ts
+++ packages/link-field/src/linkField.ts
@@ -1,39 +1,43 @@
-import type { GroupField } from 'payload'
+import type { GroupField } from "payload";
 
-import {
-  LINK_FIELD_ADMIN_COMPONENT,
-  LINK_FIELD_MARKER,
-  type LinkFieldConfig,
-} from './types.js'
-import { createLinkFields } from './linkFields.js'
+import { LINK_FIELD_ADMIN_COMPONENT, LINK_FIELD_MARKER, type LinkFieldConfig } from "./types.js";
+import { createLinkFields } from "./linkFields.js";
 
 export const linkField = ({
-  appearance = 'drawer',
-  defaultType = 'custom',
-  label,
-  name,
-  relationTo,
-  required = false,
-  showLabel = true,
-  showNewTab = true,
+	appearance = "drawer",
+	defaultType = "custom",
+	label,
+	name,
+	relationTo,
+	required = false,
+	showLabel = true,
+	showNewTab = true,
+	localizeLabel = true,
 }: LinkFieldConfig): GroupField => {
-  return {
-    name,
-    type: 'group',
-    admin: {
-      components: {
-        Field: LINK_FIELD_ADMIN_COMPONENT,
-      },
-      custom: {
-        linkField: {
-          appearance,
-          marker: LINK_FIELD_MARKER,
-          showLabel,
-          showNewTab,
-        },
-      },
-    },
-    fields: createLinkFields({ defaultType, relationTo, required, showLabel, showNewTab }),
-    label,
-  }
-}
+	return {
+		name,
+		type: "group",
+		admin: {
+			components: {
+				Field: LINK_FIELD_ADMIN_COMPONENT,
+			},
+			custom: {
+				linkField: {
+					appearance,
+					marker: LINK_FIELD_MARKER,
+					showLabel,
+					showNewTab,
+				},
+			},
+		},
+		fields: createLinkFields({
+			defaultType,
+			relationTo,
+			required,
+			showLabel,
+			showNewTab,
+			localizeLabel,
+		}),
+		label,
+	};
+};
